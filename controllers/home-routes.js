f58e6618a6d5ee9455ddb8155cac7e63ffd766f6
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-09-13 02:38:50 -0400

    added dataValues to the handlebars to successfully return post information.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
-const { Post } = require('../models');
+const { Post, User } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    // Fetch posts from the database
-    const posts = await Post.findAll();
-    console.log(posts)
-
+    // Fetch posts from the database along with their authors
+    const posts = await Post.findAll({
+      include: [{ model: User, attributes: ['username'] }]
+    });
     // Render the 'home' template with the 'posts' data
     res.render('home', { posts });
+    console.log(posts)
   } catch (err) {
     res.status(500).json(err);
   }
