d02240c52de664d0291330d2ca057d72b9c85bd7
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-09-14 12:02:42 -0400

    successfully established a GET route for posts, executed in the /api/post-routes.js file.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,8 @@ const postRoutes = require('./post-routes.js');
 const userRoutes = require('./user-routes.js');
 // const replyRoutes = require('./reply-routes.js')
 
-router.use('/postroute', postRoutes);
-router.use('/userroute', userRoutes);
 router.use('/posts', postRoutes);
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 // router.use('/replies', replyRoutes);
 
 module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,35 +3,13 @@ const router = express.Router();
 const { User, Post, Reply } = require('../../models');
 
 // // Route to retrieve posts and their associated replies with usernames
-router.get('/posts', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     // Fetch posts from the database along with their authors' usernames and replies
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['username'],
-        },
-        {
-          model: Reply,
-          attributes: ['replies_content', 'updated_at'],
-          include: [
-            {
-              model: User, // Includes the User model to get 'username' for the reply
-              attributes: ['username'],
-            },
-          ],
-        },
-      ],
-    });
-
-    // Serialize the data (flatten it) for better use in your template
+    const postData = await Post.findAll();
     const posts = postData.map((post) => post.get({ plain: true }));
-
-    // Send over the serialized data to the 'home' template
-    // loggedIn: req.session.loggedIn,
-    res.render('home', { posts });
-    console.log(posts);
+    res.json(posts);
+    // console.log(posts);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -39,7 +17,7 @@ router.get('/posts', async (req, res) => {
 
 
 // Route to create a new post
-router.post('/posts', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const newPost = await Post.create({
             title: req.body.title,
