c775abae2eac2baea766c657599541c0d31b1b69
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-09-20 03:53:30 -0400

    continued to focus on UI/UX for home and dashboard. got most of the api calls to work on dashboard.handlebars. completed post routes for posts and replies.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,32 +1,58 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Reply } = require('../models'); // Ensure you import Reply
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
-  try {
-    // Fetch all the posts by the logged-in user
-    const postData = await Post.findAll({
-      where: {
-        user_id: req.session.user_id
-      },
-      attributes: ['id', 'title', 'post_content', 'created_at'],
-      include: [
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    });
+    try {
+        // Fetch all the posts by the logged-in user
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.userId
+            },
+            attributes: ['id', 'title', 'post_content', 'created_at'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
 
-    const posts = postData.map(post => post.get({ plain: true }));
+        const posts = postData.map(post => post.get({ plain: true }));
 
-    res.render('dashboard', {
-      posts,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+        // Fetch all the posts for which the logged-in user has commented
+        const postDataWithReplies = await Post.findAll({
+            include: [
+                {
+                    model: Reply,
+                    as: 'replies',
+                    where: {
+                        user_id: req.session.userId
+                    },
+                    attributes: ['replies_content'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+
+        const postsWithReplies = postDataWithReplies.map(post => post.get({ plain: true }));
+
+        res.render('dashboard', {
+            posts,
+            postsWithReplies,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- public/js/edit_reply.js
+++ public/js/edit_reply.js
@@ -1,61 +1,61 @@
-$(document).ready(function() {
+$(document).on('click', '.editReplyButton', function() {
+    console.log("Edit reply button clicked.");
 
-    $(document).on('click', '.editReplyButton', function() {
-        console.log("Edit reply button clicked.");
+    $.ajax({
+        type: "GET",
+        url: "/api/check-auth",
+        success: function(response) {
+            console.log("Authenticated and trying to slide down comment form.");
 
-        $.ajax({
-            type: "GET",
-            url: "/api/check-auth",
-            success: function(response) {
-                console.log("Authenticated and trying to slide down comment form.");
-                const form = $(this).closest('.primary-comment-container').find('.editCommentForm').slideDown(); // Targeting the related commentForm
-                if (form.length) {
-                    console.log("Comment update form found.");
-                    form.slideDown();
-                } else {
-                    console.log("Comment update form not found.");
-                }
-                $(this).hide();
-            }.bind(this), // Binding `this` to ensure it's referring to the clicked button in the success callback
-            error: function(error) {
-                if (error.status === 401) {
-                    $('#loginModal').modal('show');
-                } else {
-                    alert('Failed to check authentication.');
-                }
+            // This targets the related commentForm inside the edit-reply-dropdown span.
+            const form = $(this).closest('[data-reply-id]').find('#edit-reply-dropdown .editCommentForm');
+            
+            if (form.length) {
+                console.log("Comment update form found.");
+                form.removeClass('d-none').slideDown();
+            } else {
+                console.log("Comment update form not found.");
             }
-        });
-    });
-
-    $(document).on('click', '.cancelButton', function() {
-        $(this).closest('.editCommentForm').slideUp();
-        $(this).closest('.primary-comment-container').find('#editReplyButton').show();
-        $(this).closest('.editCommentForm').find('.editComment_content').val("");
-    });
-
-    $(document).on('submit', '.editCommentForm', function(e) {
-        e.preventDefault();
-
-        const commentContent = $(this).find('textarea[name="editComment_content"]').val();
-        const replyID = $(this).closest('.comments-dropdown').data('reply-id');
-
-        $.ajax({
-            type: "PUT",
-            url: "/api/replies/" + replyID, // Add postID in the URL
-            data: {
-                replies_content: commentContent
-            },
-            success: function(response) {
-                location.reload();
-                alert('Comment posted successfully!');
-            },
-            error: function(error) {
-                if (error.status === 401) {
-                    $('#loginModal').modal('show');
-                } else {
-                    alert('Failed to post the comment.');
-                }
+            $(this).hide();
+        }.bind(this),
+        error: function(error) {
+            if (error.status === 401) {
+                $('#loginModal').modal('show');
+            } else {
+                alert('Failed to check authentication.');
             }
-        });
+        }
+    });
+});
+
+$(document).on('click', '.cancelButton', function() {
+    $(this).closest('#edit-reply-dropdown').find('.editCommentForm').slideUp();
+    $(this).closest('[data-reply-id]').find('.editReplyButton').show();
+    $(this).closest('.editCommentForm').find('textarea[name="editComment_content"]').val("");
+});
+
+$(document).on('submit', '.editCommentForm', function(e) {
+    e.preventDefault();
+
+    const commentContent = $(this).find('textarea[name="editComment_content"]').val();
+    const replyID = $(this).closest('[data-reply-id]').data('reply-id');
+
+    $.ajax({
+        type: "PUT",
+        url: "/api/replies/" + replyID, // Add postID in the URL
+        data: {
+            replies_content: commentContent
+        },
+        success: function(response) {
+            location.reload();
+            alert('Comment updated successfully!');
+        },
+        error: function(error) {
+            if (error.status === 401) {
+                $('#loginModal').modal('show');
+            } else {
+                alert('Failed to update the comment.');
+            }
+        }
     });
 });
